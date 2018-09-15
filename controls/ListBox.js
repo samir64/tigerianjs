4973722fdb66644c4380d2f5f1a384116b055150
samir64 <mail6410@gmail.com>
2018-09-15 18:35:59 +0430

    Added list controls

---
+++ controls/ListBox.js
@@ -0,0 +1,65 @@
+/**
+ * Created by samir on 11/10/16.
+ */
+
+"use strict";
+
+
+/**
+ * @extends {Tigerian.Control}
+ * @implements {Tigerian.BSelectGroup}
+ * @constructor
+ */
+Tigerian.ListBox = Tigerian.Control.extend({
+    /**
+     * @constructs
+     * @param {Tigerian.UI} parent
+     * @param {string} theme
+     */
+    init: function (parent, theme) {
+        this.super(parent, theme);
+        this.config("group");
+        this.config("select_group");
+
+        //NOTE Private Variables
+        var instance = this;
+
+        //NOTE Alias Super Members
+        var superAddControl = this.addControl.bind(this);
+        var bGroupSort = this.sort.bind(this);
+
+        //NOTE Attributes
+        this.setAttribute("element-type", "ListBox");
+        this.setAttribute("element-name", "container");
+
+        //NOTE Public Functions
+        /**
+         * @param {Tigerian.ListItem|string} item
+         */
+        this.addControl = this.addItem = function (item) {
+            if (Tigerian.Class.isInstance(item, "string")) {
+                item = new Tigerian.ListItem(null, item, this.theme);
+            } else if (!(Tigerian.Class.isInstance(item, Tigerian.Control) && item["Behavior:select"])) {
+                item = new Tigerian.ListItem(null, "Item " + (this.itemCount + 1).toString(), this.theme);
+            }
+
+            item.autoDeselect = this.multiSelect;
+            superAddControl(item);
+        };
+
+        /**
+         * @param {boolean} accending = true
+         */
+        this.sort = function (ascending) {
+            /**
+             * @param {Tigerian.ListItem} a 
+             * @param {Tigerian.ListItem} b 
+             */
+            function sort(a, b) {
+                return ((ascending === false) ? b.text.padNumbers(20, 20) >= a.text.padNumbers(20, 20) : a.text.padNumbers(20, 20) >= b.text.padNumbers(20, 20));
+            }
+
+            bGroupSort(sort);
+        };
+    }
+}, Tigerian.BGroup, Tigerian.BSelectGroup);
---
+++ controls/ListItem.js
@@ -0,0 +1,55 @@
+/**
+ * Created by samir on 11/7/16.
+ */
+
+"use strict";
+
+
+/**
+ * @extends {Tigerian.Label}
+ * @implements {Tigerian.BSelect}
+ * @implements {Tigerian.BText}
+ * @constructor
+ */
+Tigerian.ListItem = Tigerian.Control.extend({
+    /**
+     * @constructs
+     * @param {Tigerian.UI} parent
+     * @param {string} text
+     * @param {string} [theme]
+     */
+    init: function (parent, text, theme) {
+        var elmItem = document.createElement("div");
+
+        this.super(parent, theme);
+        this.config("select");
+        this.config("text", elmItem);
+
+        this.addControl(elmItem);
+
+        // if (Tigerian.Class.isInstance(text, "string")) {
+        //     elmItem.innerHTML = text;
+        // }
+        this.text = text;
+
+        //NOTE Attributes
+        this.setAttribute("element-type", "ListItem");
+        this.setAttribute("element-name", "container");
+
+        elmItem.setAttribute("element-type", "ListItem");
+        elmItem.setAttribute("element-name", "item");
+
+        // Object.defineProperty(this, "text", {
+        //     enumerable: true,
+        //     configurable: true,
+        //     get: function () {
+        //         return elmItem.innerHTML;
+        //     },
+        //     set: function (v) {
+        //         if (Tigerian.Class.isInstance(v, "string")) {
+        //             elmItem.innerHTML = v;
+        //         }
+        //     },
+        // });
+    }
+}, Tigerian.BSelect, Tigerian.BText);
