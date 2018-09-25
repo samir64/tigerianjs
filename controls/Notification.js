78e8c187bcaf3e182aec07368d50c0b1bf2d6b7e
samir64 <mail6410@gmail.com>
2018-09-25 20:54:44 +0330

    Added notification control

---
+++ controls/Notification.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * @constructor
+ * @extends {Tigerian.Control}
+ * @implements {Tigerian.BClose}
+ */
+Tigerian.Notification = Tigerian.Control.extend({
+    /**
+     * @constructs
+     * @param {Tigerian.UI} parent
+     * @param {string} text = ""
+     * @param {string} theme = ""
+     */
+    init: function (parent, text, theme) {
+        var elmMessage = document.createElement("div");
+
+        this.super(parent, theme);
+        this.config("text", elmMessage);
+        this.config("cancel");
+
+        this.setAttribute("element-type", "Notification");
+        this.setAttribute("element-name", "container");
+
+        elmMessage.setAttribute("element-type", "Notification");
+        elmMessage.setAttribute("element-name", "message");
+
+        this.addControl(elmMessage);
+        this.text = text;
+    },
+}, Tigerian.BText, Tigerian.BCancel);
